0ae1d491e46599ace7f59064a9baf76da8776c83
Ben Redmond <73973708+BenRedmondgithub@users.noreply.github.com>
2024-11-14 09:28:10 +0000

    Add files via upload

---
+++ D and D dice rolls/D and D dice rolls/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace D_and_D_dice_rolls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.d6button = new System.Windows.Forms.Button();
+            this.d10button = new System.Windows.Forms.Button();
+            this.d20button = new System.Windows.Forms.Button();
+            this.Outputlabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(327, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dice Rolller";
+            // 
+            // d6button
+            // 
+            this.d6button.Location = new System.Drawing.Point(334, 156);
+            this.d6button.Name = "d6button";
+            this.d6button.Size = new System.Drawing.Size(75, 63);
+            this.d6button.TabIndex = 2;
+            this.d6button.Text = "D6";
+            this.d6button.UseVisualStyleBackColor = true;
+            this.d6button.Click += new System.EventHandler(this.d6button_Click);
+            // 
+            // d10button
+            // 
+            this.d10button.Location = new System.Drawing.Point(440, 156);
+            this.d10button.Name = "d10button";
+            this.d10button.Size = new System.Drawing.Size(75, 63);
+            this.d10button.TabIndex = 3;
+            this.d10button.Text = "D10";
+            this.d10button.UseVisualStyleBackColor = true;
+            this.d10button.Click += new System.EventHandler(this.d10button_Click);
+            // 
+            // d20button
+            // 
+            this.d20button.Location = new System.Drawing.Point(549, 156);
+            this.d20button.Name = "d20button";
+            this.d20button.Size = new System.Drawing.Size(75, 63);
+            this.d20button.TabIndex = 4;
+            this.d20button.Text = "D20";
+            this.d20button.UseVisualStyleBackColor = true;
+            this.d20button.Click += new System.EventHandler(this.d20button_Click);
+            // 
+            // Outputlabel
+            // 
+            this.Outputlabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Outputlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Outputlabel.Location = new System.Drawing.Point(334, 252);
+            this.Outputlabel.Name = "Outputlabel";
+            this.Outputlabel.Size = new System.Drawing.Size(290, 41);
+            this.Outputlabel.TabIndex = 5;
+            this.Outputlabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(328, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 36);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter dice to roll";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::D_and_D_dice_rolls.Properties.Resources._6831868;
+            this.pictureBox1.Location = new System.Drawing.Point(39, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(428, 390);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(87, 48);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(428, 339);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(87, 45);
+            this.resetButton.TabIndex = 8;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Outputlabel);
+            this.Controls.Add(this.d20button);
+            this.Controls.Add(this.d10button);
+            this.Controls.Add(this.d6button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "D&D Dice Rolller";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button d6button;
+        private System.Windows.Forms.Button d10button;
+        private System.Windows.Forms.Button d20button;
+        private System.Windows.Forms.Label Outputlabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ D and D dice rolls/D and D dice rolls/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D_and_D_dice_rolls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void d6button_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int randomnumber = rnd.Next(1,7);
+            Outputlabel.Text = "you got: " + randomnumber.ToString();
+        }
+
+        private void d10button_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int randomnumber = rnd.Next(1, 11);
+            Outputlabel.Text = "you got: " + randomnumber.ToString();
+        }
+
+        private void d20button_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int randomnumber = rnd.Next(1, 21);
+            Outputlabel.Text = "you got: " + randomnumber.ToString();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            Outputlabel.Text = (" ");
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
